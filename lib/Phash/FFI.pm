3dc9fb4d1168906b6329dec1c3803c4c1841dd8b
Kang-min Liu <gugod@gugod.org>
2015-01-21 22:56:09 +0100

    some documentation.

--- lib/Phash/FFI.pm
+++ lib/Phash/FFI.pm
@@ -1,6 +1,8 @@
 package Phash::FFI;
+# ABSTRACT: FFI-based pHash interface.
 use strict;
 use warnings;
+our $VERSION = "0.01";
 
 use FFI::Raw;
 use FFI::CheckLib;
@@ -25,3 +27,34 @@ sub dct_imagehash {
 }
 
 1;
+
+=head1 NAME
+
+Phash::FFI
+
+=head1 DESCRIPTION
+
+Phash::FFI is the library adaptor for L<pHash|http://phash.org/>, which allows you to generate a hash value from media
+field (image, video, audio). It is designed so that if 2 media files have identical hash value if they are perceptually
+the same.
+
+=head1 FUNCTIONS
+
+=over 4
+
+=item dct_imagehash($file_path)
+
+This subroutine takes a path name and returns a 64-bit phash. For example:
+
+    my $file = $ARGV[0];
+    my $hash = Phash::FFI::dct_imagehash($file);
+    printf "%064b\t%s\n", $hash, $file;
+
+=back
+
+=head1 LICENSE
+
+Phash::FFI is released under MIT License.
+
+L<pHash|http://phash.org/> is released under GPLv3 license.
+
